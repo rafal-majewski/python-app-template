ee94e02a2f00d2fb8078a50a8e660bf2ad7e1040
Rafał Majewski <goodheropl@gmail.com>
2022-11-15 23:13:29 +0100

    AppConfig using pydantic

--- python_app_template/__main__.py
+++ python_app_template/__main__.py
@@ -1,5 +1,15 @@
+import pydantic
+
+
+class AppConfig(pydantic.BaseSettings):
+    # required_setting: int
+    optional_setting: int = 123
+
+
 def main() -> None:
     print("Hello World!")
+    app_config = AppConfig(_env_file=".env")
+    print(app_config.optional_setting)
 
 
 if __name__ == "__main__":
